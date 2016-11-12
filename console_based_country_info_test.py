72577e6b9748a269f1c645470176125fda5b3930
GathuBoswell <gathudev@gmail.com>
2016-11-12 18:46:58 +0300

    Improved the test on specific country data

--- console_based_country_info_test.py
+++ console_based_country_info_test.py
@@ -9,12 +9,12 @@ class CountryInfoTestCase(unittest.TestCase):
 
     def test_all_countries_info(self):
         countries_bordering_morocco = CountryInfoSearch('all')
-        self.assertEqual(countries_bordering_morocco.all_country_info(), 'https://restcountries.eu/rest/v1/all')
+        self.assertEqual(countries_bordering_morocco.all_countries_info(), 'https://restcountries.eu/rest/v1/all')
 
     def test_specific_country_data(self):
-        kenya_data = CountryInfoSearch('Kenya')
-        all_kenya_data = kenya_data.specific_country_data()
-        self.assertEqual(all_kenya_data, CountryInfoSearch('https://restcountries.eu/rest/v1/name/Kenya?fullText=true'))
+        kenya_data = CountryInfoSearch('name/Kenya?fullText=true')
+        all_kenya_data = kenya_data.specific_country_info.get_requested_data()
+        self.assertEqual(all_kenya_data[0]['name'], 'Kenya', msg='The country should be `Kenya`')
 
 
 if __name__ == '__main__':
