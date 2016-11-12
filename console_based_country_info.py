833d769399a205d0b84a788e0c3ea1e73665a67b
GathuBoswell <gathudev@gmail.com>
2016-11-12 18:49:19 +0300

    added functionality for getting data  for a specific country

--- console_based_country_info.py
+++ console_based_country_info.py
@@ -8,6 +8,11 @@ class CountryInfoSearch(object):
             self._base_url += self._options
             return self._base_url
 
+    @property
+    def specific_country_info(self):
+        self._base_url += self._options
+        return self
+
     def get_requested_data(self):
         import requests
         response = requests.get(self._base_url)
