aea5a41008f06fc14821a463e83b1f8b06943516
GathuBoswell <gathudev@gmail.com>
2016-11-12 20:34:47 +0300

    improved my get_country_info_by_country_codes by checking for list, where a list will have more than one code, while single code will just be an str

--- console_based_country_info_test.py
+++ console_based_country_info_test.py
@@ -16,7 +16,12 @@ class CountryInfoTestCase(unittest.TestCase):
         all_kenya_data = kenya_data.specific_country_info.get_requested_data()
         self.assertEqual(all_kenya_data[0]['name'], 'Kenya', msg='The country should be `Kenya`')
 
-    def test_all_countries_info_by_lang(self):
+    def test_all_countries_info_by_single_lang(self):
+        countries_code_search = CountryInfoSearch('rus')
+        resulting_url = countries_code_search.get_country_info_by_country_codes.get_url
+        self.assertEqual(resulting_url, 'https://restcountries.eu/rest/v1/alpha?codes=rus')
+
+    def test_all_countries_info_by_multiple_lang(self):
         countries_code_search = CountryInfoSearch(['rus', 'co', 'no', 'aw'])
         resulting_url = countries_code_search.get_country_info_by_country_codes.get_url
         self.assertEqual(resulting_url, 'https://restcountries.eu/rest/v1/alpha?codes=rus;co;no;aw')
