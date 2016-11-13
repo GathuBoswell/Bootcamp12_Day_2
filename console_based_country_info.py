a6013c078829220c0e022b18e62a184164aacd96
GathuBoswell <gathudev@gmail.com>
2016-11-13 08:18:17 +0300

    Added support for search by region, capital, currency and subregion

--- console_based_country_info.py
+++ console_based_country_info.py
@@ -32,9 +32,9 @@ class CountryInfoSearch(object):
         return self
 
     @property
-    def get_country_info_by_country_codes(self):
-        base_url = 'https://restcountries.eu/rest/v1/alpha?codes={}'
-        self._base_url = base_url.format(';'.join(self._options))
+    def get_country_info_by_country_currency(self):
+        base_url = 'https://restcountries.eu/rest/v1/currency/{}'
+        self._base_url = base_url.format(self._options)
         return self
 
     @property
@@ -43,6 +43,22 @@ class CountryInfoSearch(object):
         if type(self._options) == list:
             self._base_url = base_url.format(';'.join(self._options))
         else:
+            base_url = 'https://restcountries.eu/rest/v1/alpha/{}'
+            self._base_url = base_url.format(self._options)
+        return self
+
+    @property
+    def get_country_info_by_country_capital(self):
+        base_url = 'https://restcountries.eu/rest/v1/capital/{}'
+        self._base_url = base_url.format(self._options)
+        return self
+
+    def get_country_info_by_region_or_sub_region(self, reg_or_sub):
+        if reg_or_sub == 'region':
+            base_url = 'https://restcountries.eu/rest/v1/region/{}'
+            self._base_url = base_url.format(self._options)
+        elif reg_or_sub == 'sub':
+            base_url = 'https://restcountries.eu/rest/v1/subregion/{}'
             self._base_url = base_url.format(self._options)
         return self
 
@@ -51,3 +67,9 @@ class CountryInfoSearch(object):
         response = requests.get(self._base_url)
         search_results = response.json()
         return search_results
+
+
+class RefinedSearch(CountryInfoSearch):
+    def __init__(self, options):
+        super().__init__(options)
+        pass
