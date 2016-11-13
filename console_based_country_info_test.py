50ec22b4d8667625fc92fe3509d681becd7cceff
GathuBoswell <gathudev@gmail.com>
2016-11-13 13:21:36 +0300

    Added test for when the url is unreachable or unavailable

--- console_based_country_info_test.py
+++ console_based_country_info_test.py
@@ -9,11 +9,11 @@ class CountryInfoTestCase(unittest.TestCase):
 
     def test_all_countries_info(self):
         countries_bordering_morocco = CountryInfoSearch('all')
-        self.assertEqual(countries_bordering_morocco.all_countries_info.get_url, 'https://restcountries.eu/rest/v1/all')
+        self.assertEqual(countries_bordering_morocco.get_all_countries_info.get_url, 'https://restcountries.eu/rest/v1/all')
 
     def test_specific_country_data(self):
         kenya_data = CountryInfoSearch('kenya')
-        all_kenya_data = kenya_data.specific_country_info.get_requested_data()
+        all_kenya_data = kenya_data.get_specific_country_info.get_requested_data()
         self.assertEqual(all_kenya_data[0]['name'], 'Kenya', msg='The country should be `Kenya`')
 
     def test_all_countries_info_by_single_lang(self):
@@ -25,6 +25,12 @@ class CountryInfoTestCase(unittest.TestCase):
         countries_code_search = CountryInfoSearch(['rus', 'co', 'no', 'aw'])
         resulting_url = countries_code_search.get_country_info_by_country_codes.get_url
         self.assertEqual(resulting_url, 'https://restcountries.eu/rest/v1/alpha?codes=rus;co;no;aw')
+
+    def test_non_availability_of_data_404(self):
+        kenya_data = CountryInfoSearch('204')
+        results = kenya_data.get_country_info_by_calling_code.get_requested_data()
+        error = kenya_data.print_data(results)
+        self.assertEqual(error, 'Data for https://restcountries.eu/rest/v1/callingcode/204 Not Found !!')
 
 if __name__ == '__main__':
     unittest.main()
