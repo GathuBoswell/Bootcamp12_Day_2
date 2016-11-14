a6337ef3255fffa1c798ff200167153fd6f0c704
GathuBoswell <gathudev@gmail.com>
2016-11-14 09:27:00 +0300

    Added support for getting min number

--- max_min_number.py
+++ max_min_number.py
@@ -12,6 +12,14 @@ def find_max_min(num_list):
     for i in range(len(num_list)):
         if num_list[i] > max_num:
             max_num = num_list[i]
+            if num_list[i] < min_num:
+                min_num = num_list[i]
+            min_num = min_num
         max_num = max_num
-    return max_num
+    return max_num, min_num
 
+def main():
+    num_list = [1,2,3,4,5,6]
+    print(find_max_min(num_list))
+
+if __name__ == '__main__':main()
