8e31b034f39103ae7100139953bef02265dbde54
GathuBoswell <gathudev@gmail.com>
2016-11-14 09:10:01 +0300

    Initial commit

---
+++ max_min_number.py
@@ -0,0 +1,10 @@
+# max_min_number.py
+
+def find_max_min(num_list):
+    """ Function to find the max and min number from
+    The given list of numbers and return them as a list
+
+    :param num_list:
+    :return [max, min]:
+    """
+    pass
