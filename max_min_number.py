e01c4777c3a00bb607a85eb16adcf0a2713cdbea
GathuBoswell <gathudev@gmail.com>
2016-11-14 10:52:44 +0300

    removed bugs and errors

--- max_min_number.py
+++ max_min_number.py
@@ -14,21 +14,24 @@ def find_max_min(num_list):
         except ValueError:
             return 'list has items that are not integers'
         for i in range(len(num_list)):
-            if type(num_list[i]) in [str, dict, tuple, set, list]:
-                continue
+            # if type(num_list[i]) in [str, dict, tuple, set, list]:
+            #     continue
             if num_list[i] > max_num:
                 max_num = num_list[i]
-                if num_list[i] < min_num:
-                    min_num = num_list[i]
+            else:
+                max_num = max_num
+            if num_list[i] < min_num:
+                min_num = num_list[i]
+            else:
                 min_num = min_num
-            max_num = max_num
+        #print(max_num, min_num)
         if max_num == min_num:
             return [len(num_list)]
-        return [max_num, min_num]
+        return [min_num, max_num]
     return num_list # return num_list if it has only one item
 def main():
-    num_list = [23,6, 96, 54, 'yhtf', 784, 287, 'gdaa', 4564, 9437, 6764]
-    num_list1 = [2,2,2,2,2,2,2,2,2,2,2]
+    num_list = [2, 2,2,2,2,2,2,2,2,2,2,2, 2]
+    num_list1 = [2,2,2,2,2,2,2,2,2,2,4,2]
     print(find_max_min(num_list))
 
 if __name__ == '__main__':main()
