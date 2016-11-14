42d5a12a57fb5b4f29d84e4afe9482888f12e700
GathuBoswell <gathudev@gmail.com>
2016-11-14 11:31:29 +0300

    added a simple functionality and error catching

--- word_count.py
+++ word_count.py
@@ -4,4 +4,13 @@ def words(word_list):
     :param word_list:
     :return {'word': word_occurrence_count}:
     """
-    pass
+    words = {}
+    for word in [w for w in word_list.split()]:
+        try:
+            if words[word]:
+                words[word] += 1
+        except KeyError:
+            words[word] = 1
+    return words
+
+print(words('¡Hola! ¿Qué tal? Привет!'))
